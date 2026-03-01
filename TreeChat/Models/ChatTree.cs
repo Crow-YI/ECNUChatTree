9d885f1f774891211483a19793566daea576dd3c
Crow_Yi <2935523808@qq.com>
2026-03-01 23:22:10 +0800

    第一次发布前修改

--- TreeChat/Models/ChatTree.cs
+++ TreeChat/Models/ChatTree.cs
@@ -11,9 +11,6 @@ namespace TreeChat.Models
         public ChatTreeNode RootNode { get; }
         public ChatTreeNode CurrentNode { get; private set; }
         public string TreeTitle { get; set; } = "新对话";
-        public int TreeID { get; }
-
-        private static int _nextTreeID = 1;
 
         public ChatTree(string? systemPrompt = null)
         {
@@ -26,24 +23,6 @@ namespace TreeChat.Models
                 RootNode = new ChatTreeNode(null, new ChatMessage("system", "你是一个有帮助的AI助手。"));
             }
             CurrentNode = RootNode;
-            TreeID = _nextTreeID++;
-        }
-
-        public ChatTreeNode CreateNewBranch(string userInput)
-        {
-            var userMessage = new ChatMessage("user", userInput);
-            var newNode = CurrentNode.AddChildNode(userMessage);
-            CurrentNode = newNode;
-            return newNode;
-        }
-
-        public bool SwitchActiveNode(int nodeID)
-        {
-            if (RootNode == null) return false;
-            var targetNode = FindNodeById(RootNode, nodeID);
-            if (targetNode == null) return false;
-            CurrentNode = targetNode;
-            return true;
         }
 
         private ChatTreeNode? FindNodeById(ChatTreeNode startNode, int nodeID)
